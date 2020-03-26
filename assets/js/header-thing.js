c5e3da5981fab82787f27d9e0c527621ada64773
bertil291utn <bertilt4@gmail.com>
2020-03-26 17:07:02 -0500

    Change directory files - Add new folder src - Add new folder js inside assets - Move out style and index file to  root directory

---
+++ assets/js/header-thing.js
@@ -0,0 +1,23 @@
+$(function () {
+    $("#encabezado").load("src/components/header.html");
+    $("#main").load("src/App.html");
+    $("#pie-de-pagina").load("src/components/footer.html");
+    $("#scroll-header").load("src/components/scroll-header.html");
+
+    var prevScrollpos = window.pageYOffset;
+    var navTwo = document.getElementById("scroll-header");
+    window.onscroll = function () {
+        var currentScrollPos = window.pageYOffset;
+        if (prevScrollpos > currentScrollPos) {
+            //if last sccroll pos is down I mean is great than the current position remove display none
+            navTwo.classList.remove("d-none");
+            if (window.pageYOffset <= 150) {
+                navTwo.classList.add("d-none")
+            }
+        } else {
+            navTwo.classList.add("d-none");
+        }
+        prevScrollpos = currentScrollPos;
+    }
+
+});
