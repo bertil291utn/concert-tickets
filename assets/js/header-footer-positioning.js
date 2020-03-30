125aef09c39f4cc0ff1692890ca215b4f435d8f5
bertil291utn <bertilt4@gmail.com>
2020-03-30 17:34:44 -0500

    Fix slinters and html validator whole project

--- assets/js/header-footer-positioning.js
+++ assets/js/header-footer-positioning.js
@@ -1,7 +1,7 @@
 $(function () {
-    //load page elements
+    // load page elements
     $("#encabezado").load("/src/components/header/header.html");
     $("#pie-de-pagina").load("/src/components/footer/footer.html");
     $("#scroll-header").load("/src/components/scroll-header/scroll-header.html");
-   
+
 });
