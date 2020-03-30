67f9822863c4a2dfa3d158ad72f89dd12ccca794
bertil291utn <bertilt4@gmail.com>
2020-03-30 13:19:59 -0500

    Add transition scrolling header - Add transiton when scroll up to show and trasitioning linear - Change folder path  scrolling navbar js file

--- src/components/scroll-header/scrolling-navbar.js
+++ src/components/scroll-header/scrolling-navbar.js
@@ -1,18 +1,20 @@
 $(function () {
     //scrolls for the navbar
     var prevScrollpos = window.pageYOffset;
-    var navTwo = document.getElementById("scroll-header");
-    navTwo.classList.add("d-none")
+    $('#scroll-header').css('visibility', 'hidden');
     window.onscroll = function () {
         var currentScrollPos = window.pageYOffset;
         if (prevScrollpos > currentScrollPos) {
             //if last sccroll pos is down I mean is great than the current position remove display none
-            navTwo.classList.remove("d-none");
+            $('#scroll-header').css('visibility', 'visible');
+            $('#scroll-header .fixed').css('top', '0');
             if (window.pageYOffset <= 150) {
-                navTwo.classList.add("d-none")
+                $('#scroll-header').css('visibility', 'hidden');
+                $('#scroll-header .fixed').css('top', '30%');
             }
         } else {
-            navTwo.classList.add("d-none");
+            $('#scroll-header').css('visibility', 'hidden');
+            $('#scroll-header .fixed').css('top', '30%');
         }
         prevScrollpos = currentScrollPos;
     }
